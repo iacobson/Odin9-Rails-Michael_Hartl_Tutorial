daa6a3a30d3e67cfb0393c6707a3af0b92ccb70c
Dorian Iacobescu <dorian.iacobescu@gmail.com>
2015-03-19 23:26:32 +0200

    add static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,24 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  # assert that a link "home" is available
   test "should get home" do
     get :home
     assert_response :success
+    # asesert that the page has a HTML tag <title>, taht contains the value "Home...."
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
 end
