c773bafd57c9d543743902ad70112eedefe709fa
Dorian Iacobescu <dorian.iacobescu@gmail.com>
2015-03-20 10:32:34 +0200

    Solved tests on chapter 3

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,24 +1,36 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+
   # assert that a link "home" is available
   test "should get home" do
     get :home
     assert_response :success
-    # asesert that the page has a HTML tag <title>, taht contains the value "Home...."
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    # asesert that the page has a HTML tag <title>, that contains the value "Home...."
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
