dacfd44dc829f0f17a8f7bb102012cfd2b7a1ed3
Dorian Iacobescu <dorian.iacobescu@gmail.com>
2015-03-22 22:43:48 +0200

    finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  
-  root 'static_pages#home'
 
-  get 'static_pages/help'
 
-  get 'static_pages/about'
+
+  root             'static_pages#home'
+
+
+  get 'signup' => 'users#new'
+
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
 
 
 end
